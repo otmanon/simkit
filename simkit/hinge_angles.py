6bf0a9ead1b2f6d1eeb90c5702ddfce94001d2ae
otmanon <otman.benchekroun@mail.utoronto.ca>
2025-12-02 12:33:44 +0100

    nice

--- simkit/hinge_angles.py
+++ simkit/hinge_angles.py
@@ -16,3 +16,35 @@ def hinge_angles(X, H):
     s = v1[:,0]*v2[:,1] - v1[:,1]*v2[:,0]  # cross product (z component)
     theta = np.arctan2(s, c)
     return theta.reshape(-1,1)
+
+
+def hinge_angle_velocities(X, V, H):
+    # Positions
+    A = X[H[:,0]]
+    B = X[H[:,1]]
+    C = X[H[:,2]]
+
+    # Velocities
+    VA = V[H[:,0]]
+    VB = V[H[:,1]]
+    VC = V[H[:,2]]
+
+    # Edges from hinge (at B)
+    e1 = A - B          # B -> A
+    e2 = C - B          # B -> C
+
+    # Time-derivatives of edges
+    de1 = VA - VB
+    de2 = VC - VB
+
+    # Squared lengths
+    L1 = np.sum(e1*e1, axis=1)
+    L2 = np.sum(e2*e2, axis=1)
+
+    # Angular velocities of each edge about B:
+    # omega = e_perp · de / ||e||^2, with e_perp = (-ey, ex)
+    w1 = (-e1[:,1] * de1[:,0] + e1[:,0] * de1[:,1]) / L1
+    w2 = (-e2[:,1] * de2[:,0] + e2[:,0] * de2[:,1]) / L2
+
+    theta_dot = w2 - w1
+    return theta_dot.reshape(-1, 1)
