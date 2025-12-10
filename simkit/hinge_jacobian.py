44b7610f8160db5a5b8abe3853de34ccd1f2775a
otmanon <otman.benchekroun@mail.utoronto.ca>
2025-12-10 16:49:55 +0100

    fixed hinge_jacobian

--- simkit/hinge_jacobian.py
+++ simkit/hinge_jacobian.py
@@ -19,29 +19,15 @@ def hinge_jacobian_compact(X, H):
         print("WARNING:dist_AB_sq is 0, leading to a divide by zero in the hinge jacobian")
     if np.any(dist_BC_sq == 0):
         print("WARNING: dist_BC_sq is 0, leading to a divide by zero in the hinge jacobian")
-    # Complex denominator for cross terms
-    dot_prod_AB_BC = (Ax - Bx)*(Bx - Cx) + (Ay - By)*(By - Cy)
-    cross_prod_AB_BC = (Ax - Bx)*(By - Cy) - (Ay - By)*(Bx - Cx)
-    complex_denom = np.maximum(dot_prod_AB_BC**2 + cross_prod_AB_BC**2, epsilon)
-    
-    # Fourth-order denominators
-
-    
-    
-    J = np.array([(Ay - By)/dist_AB_sq, 
-              (-Ax + Bx)/dist_AB_sq,
-              (-(Ay - Cy)*dot_prod_AB_BC - cross_prod_AB_BC*(Ax - 2*Bx + Cx))/complex_denom, 
-              ((Ax - Cx)*dot_prod_AB_BC - cross_prod_AB_BC*(Ay - 2*By + Cy))/complex_denom, 
-              (By - Cy)/dist_BC_sq, 
-              (-Bx + Cx)/dist_BC_sq]).T
 
 
-    # J = np.array([(Ay - By)/(Ax**2 - 2*Ax*Bx + Ay**2 - 2*Ay*By + Bx**2 + By**2), 
-    #         (-Ax + Bx)/(Ax**2 - 2*Ax*Bx + Ay**2 - 2*Ay*By + Bx**2 + By**2),
-    #         (-(Ay - Cy)*((Ax - Bx)*(Bx - Cx) + (Ay - By)*(By - Cy)) - ((Ax - Bx)*(By - Cy) - (Ay - By)*(Bx - Cx))*(Ax - 2*Bx + Cx))/(((Ax - Bx)*(Bx - Cx) + (Ay - By)*(By - Cy))**2 + ((Ax - Bx)*(By - Cy) - (Ay - By)*(Bx - Cx))**2), 
-    #         ((Ax - Cx)*((Ax - Bx)*(Bx - Cx) + (Ay - By)*(By - Cy)) - ((Ax - Bx)*(By - Cy) - (Ay - By)*(Bx - Cx))*(Ay - 2*By + Cy))/(((Ax - Bx)*(Bx - Cx) + (Ay - By)*(By - Cy))**2 + ((Ax - Bx)*(By - Cy) - (Ay - By)*(Bx - Cx))**2), 
-    #         (By - Cy)/(Bx**2 - 2*Bx*Cx + By**2 - 2*By*Cy + Cx**2 + Cy**2), 
-    #         (-Bx + Cx)/(Bx**2 - 2*Bx*Cx + By**2 - 2*By*Cy + Cx**2 + Cy**2)]).T
+
+    J = np.array([(Ay - By)/(Ax**2 - 2*Ax*Bx + Ay**2 - 2*Ay*By + Bx**2 + By**2), 
+            (-Ax + Bx)/(Ax**2 - 2*Ax*Bx + Ay**2 - 2*Ay*By + Bx**2 + By**2),
+            (-(Ay - Cy)*((Ax - Bx)*(Bx - Cx) + (Ay - By)*(By - Cy)) - ((Ax - Bx)*(By - Cy) - (Ay - By)*(Bx - Cx))*(Ax - 2*Bx + Cx))/(((Ax - Bx)*(Bx - Cx) + (Ay - By)*(By - Cy))**2 + ((Ax - Bx)*(By - Cy) - (Ay - By)*(Bx - Cx))**2), 
+            ((Ax - Cx)*((Ax - Bx)*(Bx - Cx) + (Ay - By)*(By - Cy)) - ((Ax - Bx)*(By - Cy) - (Ay - By)*(Bx - Cx))*(Ay - 2*By + Cy))/(((Ax - Bx)*(Bx - Cx) + (Ay - By)*(By - Cy))**2 + ((Ax - Bx)*(By - Cy) - (Ay - By)*(Bx - Cx))**2), 
+            (By - Cy)/(Bx**2 - 2*Bx*Cx + By**2 - 2*By*Cy + Cx**2 + Cy**2), 
+            (-Bx + Cx)/(Bx**2 - 2*Bx*Cx + By**2 - 2*By*Cy + Cx**2 + Cy**2)]).T
     return J
 
 
