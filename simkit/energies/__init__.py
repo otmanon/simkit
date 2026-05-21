d0d90b40490032a883fa4436e32ca0da47fe5f6d
otmanon <otman.benchekroun@mail.utoronto.ca>
2026-05-21 09:05:10 -0400

    updated __init__.py with new elastic

--- simkit/energies/__init__.py
+++ simkit/energies/__init__.py
@@ -1,8 +1,4 @@
-from .elastic_energy import  elastic_energy_x, elastic_energy_S, elastic_energy_z, ElasticEnergyZPrecomp
-from .elastic_gradient import elastic_gradient_dF, elastic_gradient_dx, elastic_gradient_dS, elastic_gradient_dz
-from .elastic_hessian import elastic_hessian_d2F, elastic_hessian_d2x, elastic_hessian_d2S, elastic_hessian_d2z
-
-
+from .elastic import *
 from .quadratic import *
 from .arap import *
 from .fcr import *
