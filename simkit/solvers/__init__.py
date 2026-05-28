335c3978ed9b93d1a2e46b1254da25465196aa63
otmanon <otman.benchekroun@mail.utoronto.ca>
2026-05-28 16:21:27 -0400

    fixed rs coordinates and removed broken membrane case

--- simkit/solvers/__init__.py
+++ simkit/solvers/__init__.py
@@ -2,6 +2,8 @@ from .Solver import Solver, SolverParams
 
 from .NewtonSolver import NewtonSolver, NewtonSolverParams
 
+from .GradientDescentSolver import GradientDescentSolver, GradientDescentSolverParams
+
 from .BlockCoordSolver import BlockCoordSolver, BlockCoordSolverParams
 
 from .CMAESSolver import CMAESSolver, CMAESSolverParams
